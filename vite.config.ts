0ae816b4f13eb2321b9184def8b1e13d4faf14d2
killoblanco <killo.blanco@gmail.com>
2024-05-23 23:13:21 -0500

    🔧 Set up docs build

--- vite.config.ts
+++ vite.config.ts
@@ -1,45 +1,7 @@
 import react from '@vitejs/plugin-react-swc'
-import { resolve, relative, extname } from 'path'
-import { fileURLToPath } from 'node:url'
-import { glob } from 'glob'
 import { defineConfig } from 'vite'
-import dts from 'vite-plugin-dts'
-import { libInjectCss } from 'vite-plugin-lib-inject-css'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    libInjectCss(),
-    dts({ include: ['lib'] })
-  ],
-  build: {
-    copyPublicDir: false,
-    lib: {
-      entry: resolve(__dirname, 'lib/index.ts'),
-      formats: ['es']
-    },
-    rollupOptions: {
-      external: ['react', 'react/jsx-runtime'],
-      input: Object.fromEntries(
-        glob.sync(['lib/**/*.{ts,tsx}', 'lib/webcraft.css'], {
-          ignore: ["lib/**/*.d.ts"],
-        }).map(file => [
-          // The name of the entry point
-          // lib/nested/foo.ts becomes nested/foo
-          relative(
-            'lib',
-            file.slice(0, file.length - extname(file).length)
-          ),
-          // The absolute path to the entry file
-          // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
-          fileURLToPath(new URL(file, import.meta.url))
-        ])
-      ),
-      output: {
-        assetFileNames: 'css/[name][extname]',
-        entryFileNames: '[name].js',
-      }
-    }
-  }
+  plugins: [react()],
 })
